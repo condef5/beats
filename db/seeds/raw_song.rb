83ae942f88ecfc27dd87da568a0c182e18995bbd
condef5 <frank@able.co>
2020-11-29 17:04:29 -0500

    Add some song seeds

---
+++ db/seeds/raw_song.rb
@@ -0,0 +1,421 @@
+records = [
+  %W[
+    https://youtu.be/dfh4JuzYNpM
+    Bleach opening 1
+  ],
+  %W[
+    https://youtu.be/a8oDEY4yoR4
+    Asian Kung-fu Generation
+  ],
+  %W[
+    https://youtu.be/mQAfG3DgG7M
+    Samurai Champloo – ending 1
+  ],
+  %W[
+    https://youtu.be/9Gj47G2e1Jc
+    Plastic Love
+  ],
+  %W[
+    https://youtu.be/2YDhu0fDlis
+    Blue - Cowboy bebop
+  ],
+  %W[
+    https://youtu.be/LkNU7AhibFM
+    Berserk first openign - Penpals
+  ],
+  %W[
+    https://youtu.be/kN0XSSdya4w
+    Inuyasha Ending 5 - Shinjitsu no uta
+  ],
+  %W[
+    https://youtu.be/SRn99oN1p_c
+    Naruto opening 2
+  ],
+  %W[
+    https://youtu.be/oPg1J79sgkQ
+    Full Metal Alchemist Opening 4 [Rewrite]
+  ],
+  %W[
+    https://youtu.be/VT6LFOIofRE
+    Libera Me From Hell
+  ],
+  %W[
+    https://youtu.be/kVxc7lhKjW4
+    Kiba Opening 1
+  ],
+  %W[
+    https://youtu.be/eFah8TCWOro
+    Attack on Titan - The Reluctant Heroes
+  ],
+  %W[
+    https://youtu.be/pFoc5XKkIIw
+    Attack on Titan - 'Call Your Name'
+  ],
+  %W[
+    https://youtu.be/7U7BDn-gU18
+    Vinland saga OP 1
+  ],
+  %W[
+    https://youtu.be/VWR_hTC4A98
+    I've Always Known | Mob Psycho 100 II
+  ],
+  %W[
+    https://youtu.be/FWDrc1nIKlA
+    Blood+ Opening 2
+  ],
+  %W[
+    https://youtu.be/cXb7M2DQiEk
+    Gangsta Opening 1
+  ],
+  %W[
+    https://youtu.be/qGMwSqQvEWk
+    GANGSTA「AMV」- Everything Black
+  ],
+  %W[
+    https://youtu.be/Rm8UjBAS3cs
+    Parasyte Opening
+  ],
+  %W[
+    https://youtu.be/9m3qAlI_29I
+    Blue Exorcist; Opening 1
+  ],
+  %W[
+    https://youtu.be/2edBgIH7y68
+    Bleach - Opening 3 | Ichirin no Hana
+  ],
+  %W[
+    https://youtu.be/uMeR2W19wT0
+    tokyo ghoul opening 1
+  ],
+  %W[
+    https://youtu.be/E-ejE--Q3UE
+    Bleach - Opening 15 | Harukaze
+  ],
+  %W[
+    https://youtu.be/TbAhjyUwH6Q
+    Ajin opening 1
+  ],
+  %W[
+    https://youtu.be/Czj0m3vZhBY
+    91 days opening
+  ],
+  %W[
+    https://youtu.be/hCzkkHwR2gg
+    DARLING in the FRANXX
+  ],
+  %W[
+    https://youtu.be/Hx_nMs-sjZg
+    Nevereverland - Nano
+  ],
+  %W[
+    https://youtu.be/1tk1pqwrOys
+    jujutsu kaisen opening amv
+  ],
+  %W[
+    https://youtu.be/sc0KHGDH8L8
+    Gantz Amv - Bonnie Pink - Last Kiss
+  ],
+  %W[
+    https://youtu.be/SnHROjvhZew
+    Full metal alchemist opening 2
+  ],
+  %W[
+    https://youtu.be/euW71JOlKy0
+    Slam Dunk op latino
+  ],
+  %W[
+    https://youtu.be/SavhHnWla6c
+    Naruto Shippuden Opening 6
+  ],
+  %W[
+    https://youtu.be/oTNYCBWO42c
+    My Hero Academia opening 2
+  ],
+  %W[
+    https://youtu.be/-eYK3YP524A
+    Soul Eater – Opening Theme 2 – Papermoon
+  ],
+  %W[
+    https://youtu.be/WWBDuVCuiUQ
+    Psycho-Pass Opening 1
+  ],
+  %W[
+    https://youtu.be/2ZN-zAKIfyQ
+    Ergo Proxy OP Full HD (Kiri - MONORAL)
+  ],
+  %W[
+    https://youtu.be/Ua5UARWRYLA
+    "Kawaki wo Ameku (カワキヲアメク)" by Minami | Domestic Girlfriend
+  ],
+  %W[
+    https://youtu.be/v0cAJ1Cg8E8
+    Gintama - Ending 17 | Samurai Heart
+  ],
+  %W[
+    https://youtu.be/Oyhpwyrq1N0
+    Naruto Ending 1 | Wind (HD)
+  ],
+  %W[
+    https://youtu.be/Ivrrt6oYxxc?list=LL
+    eery - Her
+  ],
+  %W[
+    https://youtu.be/WofLTwSi0uo
+    Opening 2 Fate Stay Night [UBW] Aimer
+  ],
+  %W[
+    https://youtu.be/cM1WSovcn4I
+    Rent-a-Girlfriend - Opening | Centimeter
+  ],
+  %W[
+    https://youtu.be/diqmC7AfEzk?list=LL
+    Kemurikusa Opening
+  ],
+  %W[
+    https://youtu.be/62uvW_lzNuc
+    [Chillstep] Sappheiros - Embrace
+  ],
+  %W[
+    https://youtu.be/RKaL146I-TE
+    Bleach - Ending 10 | Sakura Biyori
+  ],
+  %W[
+    https://youtu.be/wP3WF94lwSs
+    Rotti - Solanin
+  ],
+  %W[
+    https://youtu.be/7O87OdsVOIE
+    Bleach - Opening 9 | Velonica
+  ],
+  %W[
+    https://youtu.be/wKMybibgt6Q
+    Bleach - Opening 8 | Chu-Bura
+  ],
+  %W[
+    https://youtu.be/6BDW1V_P5sA
+    ハイブリッドレインボウ／the pillows
+  ],
+  %W[
+    https://youtu.be/AFnF2E6nJDw
+    Inuyashiki OP - My Hero
+  ],
+  %W[
+    https://youtu.be/HwSZBbYiQ-o
+    b the beginning ending/opening
+  ],
+  %W[
+    https://youtu.be/lilv4MvBY6E
+    Naruto Opening 5 | Seishun Kyousoukyoku
+  ],
+  %W[
+    https://youtu.be/XDpoBc8t6gE
+    kudasai - the girl i haven't met
+  ],
+  %W[
+    https://youtu.be/UlrhACu5bxQ
+    Mushishi AMV - Shiver(Lucy Rose)
+  ],
+  %W[
+    https://youtu.be/9StfX1p9LuY
+    Opening 2 Psycho Pass Nothings
+  ],
+  %W[
+    https://youtu.be/7XtilO0l8L4
+    Banana Fish : Opening 1
+  ],
+  %W[
+    https://youtu.be/aATPWZ-FpJ8
+    Ending 1 Dr. Stone
+  ],
+  %W[
+    https://youtu.be/JBqxVX_LXvk
+    Fire Force – Opening Theme – Inferno
+  ],
+  %W[
+    https://youtu.be/B7lOojsJ8Ts
+    Answer is Near Sub Español S.A.O AMV
+  ],
+  %W[
+    https://youtu.be/vxvP9zSOL7s
+    Naruto Shippuden Opening 1
+  ],
+  %W[
+    https://youtu.be/QOJxhxiM0eQ
+    Fairy tail op 1; Snow fairy (Sub español)
+  ],
+  %W[
+    https://youtu.be/TaBc7jc-bF8
+    Abnormalize
+  ],
+  %W[
+    https://youtu.be/PGoHzoZ2OI0
+    Durarara!! Opening 2
+  ],
+  %W[
+    https://youtu.be/lQCp8yFbSrI
+    Bob Lennon (gutalala sudalala)
+  ],
+  %W[
+    https://youtu.be/osKkMKolWQ8
+    Code Geass R2 Textless Opening 2
+  ],
+  %W[
+    https://youtu.be/Bw-5Lka7gPE
+    Mob Psycho 100 - Opening | 99
+  ],
+  %W[
+    https://youtu.be/Rol_148PqjI
+    Fate Zero opening 1 full
+  ],
+  %W[
+    https://youtu.be/YBhsDtFUgdM
+    Code Geass Opening 2 HD
+  ],
+  %W[
+    https://youtu.be/AgWI4EQ78ik
+    Code Geass R2 Opening 1
+  ],
+  %W[
+    https://youtu.be/x9nkiTeuYRo
+    ASIAN KUNG-FU GENERATION - Blue Train
+  ],
+  %W[
+    https://youtu.be/yDETVPFdbT4
+    Whatever happens, happens... // Verzache
+  ],
+  %W[
+    https://youtu.be/KQj0eXtsObU
+    Dr. STONE - Ending Full『LIFE』
+  ],
+  %W[
+    https://youtu.be/O9SBIdzQZes
+    The Pillows - White Summer And Green Bicycle
+  ],
+  %W[
+    https://youtu.be/JXkq0JWGMM8
+    Free op 1
+  ],
+  %W[
+    https://youtu.be/X59yPeVk_70
+    Fullmetal Alchemist Brotherhood Opening 1
+  ],
+  %W[
+    https://youtu.be/Eq6EYcpWB_c
+    Samurai Champloo Opening 1
+  ],
+  %W[
+    https://youtu.be/dd7BILZcYAY
+    Steins;Gate Opening HD 1080p
+  ],
+  %W[
+    https://youtu.be/FgQR2O8PGf4
+    Dororo op1
+  ],
+  %W[
+    https://youtu.be/QahFijEd3hY
+    Golden Kamuy – Opening Theme
+  ],
+  %W[
+    https://youtu.be/cRA5gsdCf4c
+    Fullmetal Alchemist Brotherhood Opening 5
+  ],
+  %W[
+    https://youtu.be/OvahxFH7IGg
+    Bleach - Opening 7 | After Dark
+  ],
+  %W[
+    https://youtu.be/lelGG_1jtcg
+    Gintama - Opening 11 | Wonderland
+  ],
+  %W[
+    https://youtu.be/edki8Su2c7U
+    Tokyo Ghoul:re Season 2 Op Full
+  ],
+  %W[
+    https://youtu.be/-VqnN_uSEgY
+    Mayonaka No Orchestra
+  ],
+  %W[
+    https://youtu.be/XXjgwpayNoM
+    Bleach - Opening 13 | Ranbu no Melody
+  ],
+  %W[
+    https://youtu.be/0xMFEE4uMgM
+    Gintama Opening 15 | Pride Kakumei (HD)
+  ],
+  %W[
+    https://youtu.be/8QE9cmfxx4s
+    Death Note OP 1
+  ],
+  %W[
+    https://youtu.be/SKWU3tyWnwA
+    Death Note Ending 1 Sub Ita (Alumina)
+  ],
+  %W[
+    https://youtu.be/8OkpRK2_gVs
+    Attack on Titan – Opening Theme
+  ],
+  %W[
+    https://youtu.be/iBRH9Po3gw8
+    My Hero Academia - Opening 4 | Odd Future
+  ],
+  %W[
+    https://youtu.be/vZ11VI2R0MM
+    OP2//Blue Exorcist - My World;
+  ],
+  %W[
+    https://youtu.be/PCmdN-Rcqq8
+    Gintama - Opening 17 v4 | Know Know Know
+  ],
+  %W[
+    https://youtu.be/jZvFEtR8RH0
+    The Rising of the Shield Hero - Opening (HD)
+  ],
+  %W[
+    https://youtu.be/ibkhGFKhyW8
+    Rainbow: Nisha Rokubō no Shichinin OP
+  ],
+  %W[
+    https://youtu.be/MWSR17vEVBw
+    Attack on Titan / Shingeki no Kyojin Season 3 OP
+  ],
+  %W[
+    https://youtu.be/ZJ-SpKD0Fq8
+    Nana Opening 2 with lyrics
+  ],
+  %W[
+    https://youtu.be/I671_pldjDU
+    NANA OP// Anna Tsuchiya - Rose; sub esp
+  ],
+  %W[
+    https://youtu.be/QxkMzn4et2U
+    Ghost in the Shell: Stand Alone Complex Opening
+  ],
+  %W[
+    https://youtu.be/O5uYQF4R-jo
+    Fate Zero Op 2 [NC]
+  ],
+  %W[
+    https://youtu.be/nU21rCWkuJw
+    Neon Genesis Evangelion (A Cruel Angel's Thesis) Anime Intro Opening
+  ],
+  %W[
+    https://youtu.be/QT9OawmZ_FY
+    One Piece Ending 1 Sub Español
+  ],
+]
+
+i = 0
+content = []
+
+content << "records:"
+records.each do |url, *name|
+  i += 1
+  content << "  - sprig_id: #{i}"
+  content << "    url: #{url}"
+  content << "    name: #{name.join(" ").dump}"
+  content << ""
+end
+
+path = File.expand_path('../shared/songs.yml', __FILE__)
+File.write(path, content.join("\n"), nil , mode: 'w')
