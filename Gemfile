6eecda7b06405ef7ae0e3a0582fb9ad90e6ebd46
condef5 <frank@able.co>
2020-12-05 21:42:08 -0500

    Get video info from link

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'jsonapi-resources'
 gem 'sprig'
+gem 'video_info'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,18 @@
 class Song < ApplicationRecord
+  # callbacks
+  before_create :set_video_info
+
   # validations
   validates :url, presence: true, uniqueness: true
+
+  private
+
+  def set_video_info
+    video_info = VideoInfo.new(url)
+    self.name = video_info.title unless self.name?
+    self.duration = video_info.duration
+    self.image = video_info.thumbnail_medium
+  end
 end
 
 # == Schema Information
