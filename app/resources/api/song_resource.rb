da6e68b0411f66486efe73b9fdab5a0f67412aeb
condef5 <frank@able.co>
2020-12-21 23:37:33 -0500

    Add user_id to songs

--- app/resources/api/song_resource.rb
+++ app/resources/api/song_resource.rb
@@ -4,4 +4,8 @@ class Api::SongResource < JSONAPI::Resource
   def self.default_sort
     [{ field: 'created_at', direction: :desc }]
   end
+
+  before_create do
+    @model.user_id = context[:current_user].id
+  end
 end
