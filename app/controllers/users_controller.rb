6d5974247eeacf9f4f679189d641167fec6b9db5
condef5 <frankcondezo@gmail.com>
2021-03-12 23:28:27 -0500

    Add flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < Clearance::UsersController
   layout "sessions"
+
+  def create
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      redirect_back_or url_after_create
+    else
+      flash[:alert] = @user.errors.full_messages.first
+      render :new
+    end
+  end
 end
