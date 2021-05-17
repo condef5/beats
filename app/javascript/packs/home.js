fa9a97bfe3c283af591db5a620ab143d229ef604
condef5 <frankcondezo@gmail.com>
2021-05-17 00:20:37 -0500

    add link to home page

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -2,7 +2,7 @@ import App from "../components/App";
 import React from "react";
 import ReactDOM from "react-dom";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("turbolinks:load", () => {
   const $app = document.querySelector("#app");
 
   ReactDOM.render(<App songs={JSON.parse($app.dataset.songs)} />, $app);
